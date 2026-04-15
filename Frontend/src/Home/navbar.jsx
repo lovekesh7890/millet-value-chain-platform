d3a5c46f404c12c00f27fdc56c0b84a6c991fc04
AnushkaSingh <anushkasingh20156@gmail.com>
2026-04-16 04:07:24 +0530

    Navbar

--- Frontend/src/Home/navbar.jsx
+++ Frontend/src/Home/navbar.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 function Navbar() {
     return (
-        <h1>navbar</h1>
+      <nav
+        className="navbar navbar-expand-lg fixed-top"
+        style={{ backgroundColor: "rgba(255, 255, 255, 0.4)" }}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand font-bold ms-5 text-8xl fs-3" href="#">
+            ShreeAnna.com
+          </a>
+        </div>
+        <form className="d-flex" role="search">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className=" nav-items me-5">
+              <a
+                href="#" className="text-decoration-none text-black fs-6 font-bold">
+                SignUp
+              </a>
+            </li>
+            <li className="nav-items text-decoration-none me-5">
+              <a
+                href="#"
+                className="text-decoration-none text-black fs-6 font-bold"
+              >
+                AboutUs
+              </a>
+            </li>
+          </ul>
+        </form>
+      </nav>
     );
 }
 
