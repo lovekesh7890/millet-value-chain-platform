2e29417d74023e7f63b8c9bee8b3576e28c5b323
AnushkaSingh <anushkasingh20156@gmail.com>
2026-04-22 12:41:00 +0530

    change

--- Frontend/src/signup.jsx
+++ Frontend/src/signup.jsx
@@ -157,7 +157,6 @@ function Signup() {
 
                 <div className="flex justify-center m-2 p-1">
                   <button
-                    onClick={handleSubmit}
                     type="submit"
                     className="bg-green-800 text-white px-4 py-2 rounded"
                   >
